eab4a1befc5abcb482fce353d1614e133810df22
Torbjørn Dahl <torbjorndahl@Torbjrns-MacBook-Pro.local>
2021-10-09 13:45:24 +0200

    Working for small datasets, add prettyprint for matrix

--- oblig2/oblig2.py
+++ oblig2/oblig2.py
@@ -1,63 +1,61 @@
 import csv
 print("Oppgave 1")
 
-graph = []
-
+# actor_tsv = open("actors.tsv")
 actor_tsv = open("actors_short.tsv")
 actors = csv.reader(actor_tsv, delimiter="\t")
 actors_arr = []
 for row in actors:
     actors_arr.append(row)
 
+# movies_tsv = open("movies.tsv")
 movies_tsv = open("movies_short.tsv")
 movies = csv.reader(movies_tsv, delimiter="\t")
 
 num_of_actors = len(actors_arr)
-Matrix = [[[] for x in range(num_of_actors)] for y in range(num_of_actors)] 
+print('Number of actors:', num_of_actors)
+
+print('Initiliazing matrix')
+# row = [[]]*num_of_actors
+Matrix = [[[] for j in range(num_of_actors)] for i in range(num_of_actors)]
+print('Done initiliazing matrix')
 
 movies_arr = []
 for row in movies:
     movies_arr.append(row)
-    # nm_id = row[0]
-    # for actor in actors_arr:
-    #     if nm_id in actor:
-    #         Matrix[]
-    #         print(actor[0], row[0])
-    #     else:
-    #         print(actor[0])
 
+def findMovie(mov, movies):
+    for movie in movies:
+        if movie[0] == mov:
+            return movie
 
-## For hver actor, Gå igjennom filmene, 
-# for hver film, finn actors som er i samme film
-# fyll inn i matrisen for den actoren.
+'''
+For hver actor (actor1), gå igjennom alle andre actors (actor2) 
+og legg til alle felles filmer i matrisen på actor2.
+'''
+
+def prettyPrintMatrix(m):
+    s = [[str(e) for e in row] for row in m]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print('\n'.join(table))
+
 for actor_i, actor in enumerate(actors_arr):
     actor_movies = actor[2:]
     for actor_i2, actor2 in enumerate(actors_arr):
         if(actor_i == actor_i2):
             continue
         actor2_movies = actor2[2:]
+        
         for mov in actor2_movies:
             if(mov in actor_movies):
-                print('yeehaw')
-                Matrix[actor_i2][actor_i].append(mov)
+                prettyPrintMatrix(Matrix)
+                movie = findMovie(mov, movies_arr)
+                print('moviename:', movie[1])
+                print(actor_i2, actor_i)
+                Matrix[actor_i2][actor_i].append(movie[0])
+                Matrix[actor_i2][actor_i].append(movie[2])
 
-
-s = [[str(e) for e in row] for row in Matrix]
-lens = [max(map(len, col)) for col in zip(*s)]
-fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-table = [fmt.format(*row) for row in s]
-print('\n'.join(table))
-
-# print(Matrix)
-        # for movie in actor_movies:
-        #     if movie in actor2:
-        #         print(movie, actor2[1])
-        #         movie_arr = []
-        #         for list in movies_arr:
-        #             if (list[0] == movie):
-        #                 movie_arr = list
-        #                 Matrix[actor_i][actor_i2].append([movie_arr[0], movie_arr[2]])
-
-# print(actors_arr[3])
-# print(Matrix[3])
+prettyPrintMatrix(Matrix)
 
