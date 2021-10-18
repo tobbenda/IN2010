705b88acdf732e2aab68bffa27ab60e8808ad393
Torbjørn Dahl <torbjorndahl@Torbjrns-MacBook-Pro.local>
2021-10-18 20:47:01 +0200

    Add tast 3, needs imporvement

--- oblig2/oblig2.py
+++ oblig2/oblig2.py
@@ -111,3 +111,69 @@ def printSolutionTask2(solution, graph):
 # solution = dijkstra(actorGraph, 'nm0000001', 'nm0000006')
 solution = dijkstra(actorGraph, 'nm0031483', 'nm0931324')
 printSolutionTask2(solution, actorGraph)
+
+
+print('Oppg3')
+def printSolutionTask3(solution, graph):
+    totalWeight = 0
+    for index, actor in enumerate(solution['path']):
+        if index < len(solution['path'])-1:
+            commonEdges = [edge for edge in graph[actor]['edges'] if edge["actorId"] == solution['path'][index+1]]
+            commonEdge = max(commonEdges, key=lambda x:x['rating'])
+            commonMovie = commonEdge['movieTitle']
+            commonMovieId = [edge for edge in graph[actor]['edges'] if edge["actorId"] == solution['path'][index+1]][0]['movieId']
+            print(graph[actor]['name'], '(', actor,')', 'via the movie', commonMovie, '(', commonMovieId, commonEdge['rating'] ,')', 'to', graph[solution['path'][index+1]]['name'])
+            totalWeight = totalWeight + (10-float(commonEdge['rating']))
+    print('Total Weight:', totalWeight)
+
+
+def dijkstraWeighted(graph, startNodeId, endNodeId):
+    # prettyPrintGraph(graph)
+    path_lengths = {node_id: {'dist': float('inf'), 'path': [startNodeId]} for node_id in graph}
+    path_lengths[startNodeId]['dist'] = 0
+    path_stack = {}
+    leftToVisit = {node for node in graph}
+    leftToVisit.remove(startNodeId)
+    visited = {startNodeId}
+
+    neighbours = [{"actorId": node['actorId'], "weight": 10-float(node['rating'])} for node in graph[startNodeId]['edges']]
+    for neighbour in neighbours:
+        if neighbour['actorId'] in path_lengths[neighbour['actorId']]['path']:
+            continue
+        path_lengths[neighbour['actorId']]['dist'] = neighbour['weight']
+        path_stack[neighbour['actorId']]= neighbour
+        path_lengths[neighbour['actorId']]['path'].append(neighbour['actorId'])
+    i= 0
+    while i < len(leftToVisit):
+        i+=1
+        pathStackList = [{"weight":path_stack[key]['weight'], "id": key} for key in path_stack]
+        nextNode = min(pathStackList, key=lambda x:x['weight'])['id']
+        nextNodeDist = path_lengths[nextNode]['dist']
+        neighbours = [{"actorId":node['actorId'], "weight": 10-float(node['rating'])} for node in graph[nextNode]['edges']]
+        for neighbour in neighbours:
+            if neighbour['actorId'] in visited:
+                continue
+            neighbourWeight = neighbour['weight']
+            if nextNodeDist+neighbourWeight < path_lengths[neighbour['actorId']]['dist']:
+                path_lengths[neighbour['actorId']]['dist'] = nextNodeDist+neighbourWeight
+                path_lengths[neighbour['actorId']]['path'] = list(path_lengths[nextNode]['path'])
+                path_lengths[neighbour['actorId']]['path'].append(neighbour['actorId'])
+            if neighbour['actorId'] == endNodeId:
+                return path_lengths[neighbour['actorId']]
+            path_stack[neighbour['actorId']] = neighbour
+        visited.add(nextNode)
+        del path_stack[nextNode]
+
+
+'''
+nm2255973 nm0000460 Donald Glover Jeremy Irons
+nm0424060 nm0000243 Scarlett Johansson Denzel Washington
+nm4689420 nm0000365 Carrie Coon Julie Delpy
+nm0000288 nm0001401 Christian Bale Angelina Jolie
+nm0031483 nm0931324 Atle Antonsen Michael K. Williams
+'''
+
+
+solution = dijkstraWeighted(actorGraph, 'nm0000001', 'nm0000006')
+solution = dijkstraWeighted(actorGraph, 'nm2255973', 'nm0000460')
+printSolutionTask3(solution, actorGraph)
